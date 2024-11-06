527049e28eb62986b844d354d6c9b81d6bcfb7da
jandib <142757988+jjjandib6013@users.noreply.github.com>
2024-11-06 21:54:14 +0800

    Tried Amlani's Idea
    Will still revert to CardLayout

--- IntroFrame.java
+++ IntroFrame.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class IntroFrame {
-    public void intro() {
-        JFrame intro = new JFrame("Who Wants To Be A Millionaire");
+    public void runUI() {
+        PlayFrame play = new PlayFrame();
 
+        JFrame intro = new JFrame("Who Wants To Be A Millionaire");
         intro.setSize(800, 800);
         intro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         intro.setLocationRelativeTo(null);
@@ -28,14 +31,28 @@ class IntroFrame {
         introPanel.add(titleLabel, BorderLayout.NORTH);
         introPanel.add(introAnimationPanel, BorderLayout.CENTER);
 
-        JPanel buttonsPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel(new FlowLayout());
         JButton playButton = new JButton("Play");
         playButton.setFont(new Font(Font.SERIF, Font.BOLD, 25)); 
         playButton.setBackground(new Color(158, 114, 195, 1)); 
         playButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 40, 0));
         playButton.setFocusable(false);
 
+        JButton replaceButton = new JButton("Replace");
+        replaceButton.setFont(new Font(Font.SERIF, Font.BOLD, 25)); 
+        replaceButton.setBackground(new Color(158, 114, 195, 1)); 
+        replaceButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 40, 0));
+        replaceButton.setFocusable(false);
+
         buttonsPanel.add(playButton);
+
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                intro.getContentPane().removeAll();
+                play.openPlayUI(intro);
+            }
+        });
 
         introPanel.add(buttonsPanel, BorderLayout.SOUTH);
 
